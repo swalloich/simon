2ae66febc645049d0657ba30983c1cae79a94448
swalloich <nels.jac42@gmail.com>
2023-03-06 21:38:29 -0700

    added light functions

--- play.js
+++ play.js
@@ -29,28 +29,27 @@ let simonSequence = new ColorSequence();
 function initiateSimon()
 {
     simonSequence.addRandomColor();
-
-}
-
-function continueSequence()
-{
-
+    playSequence();
 }
 
 function playSequence()
 {
     let sequenceArr = simonSequence.getSequence();
-    for (let i = 0; i < sequenceArr.getSequence().length; i++)
+    for (let i = 0; i < sequenceArr.length; i++)
     {
         switch (sequenceArr[i])
         {
             case 0:
+                setTimeout(lightTL, 500);
                 break;
             case 1:
+                setTimeout(lightTR, 500);
                 break;
             case 2:
+                setTimeout(lightBL, 500);
                 break;
             case 3:
+                setTimeout(lightBR, 500);
                 break;
         }
     }
@@ -58,20 +57,36 @@ function playSequence()
 
 function lightTL()
 {
-    set
+    const simonTL = document.getElementById('simon-tl');
+    simonTL.style.backgroundColor = "var(--simon-tl-light)";
+    setTimeout(() => {
+        simonTL.style.backgroundColor = "var(--simon-tl-dark)";
+    }, 1000);
 }
 
 function lightTR()
 {
-
+    const simonTR = document.getElementById('simon-tr');
+    simonTR.style.backgroundColor = "var(--simon-tr-light)";
+    setTimeout(() => {
+        simonTR.style.backgroundColor = "var(--simon-tr-dark)";
+    }, 1000);
 }
 
 function lightBL()
 {
-
+    const simonBL = document.getElementById('simon-bl');
+    simonBL.style.backgroundColor = "var(--simon-bl-light)";
+    setTimeout(() => {
+        simonBL.style.backgroundColor = "var(--simon-bl-dark)";
+    }, 1000);
 }
 
 function lightBR()
 {
-
+    const simonBR = document.getElementById('simon-br');
+    simonBR.style.backgroundColor = "var(--simon-br-light)";
+    setTimeout(() => {
+        simonBR.style.backgroundColor = "var(--simon-br-dark)";
+    }, 1000);
 }
