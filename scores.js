d186c919411aed2bce317a042b8f787ee9a9929b
swalloich <nels.jac42@gmail.com>
2023-03-07 00:09:50 -0700

    worked on scores.js

--- scores.js
+++ scores.js
@@ -1,29 +1,56 @@
 class Scores
 {
-    static getScores(uname)
-    {
-        return false;
-    }
-
-    static writeScore(uname, score)
-    {
-        
-    }
-
-    #getScoreCookie()
+    static getScores()
     {
         let cookies = document.cookie.split(';');
+        let hasScores = false;
+
         for (let i = 0; i < cookies.length; i++)
         {
             if (cookies[i].includes('scores'))
             {
-                return cookies[i].split('=');
+                hasScores = true;
+                return cookies[i].split('=')[1];
             }
         }
     }
 
-    #scoreJson(jsonString)
+    static writeScore(score)
     {
-        return JSON.parse(jsonString);
+        let cookies = document.cookie.split(';');
+        let hasScores = false;
+
+        for (let i = 0; i < cookies.length; i++)
+        {
+            if (cookies[i].includes('scores'))
+            {
+                hasScores = true;
+            }
+        }
+
+        if (hasScores == true)
+        {
+            // get json
+            // decode into object array
+            // add new score
+            // write it
+        }
+        else
+        {
+            // encode scores as json array
+            // write json array as cookie
+        }
     }
+}
+
+function writeScore()
+{
+    const scoreElem = document.getElementById('score');
+    Scores.writeScore(scoreElem.value);
+}
+
+function displayScores()
+{
+    const displayElem = document.getElementById('score-table');
+    // use json to make a table containing scores
 }
