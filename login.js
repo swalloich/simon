c39a0f91c7a4b5066e126cfe09147db3caa84a84
swalloich <nels.jac42@gmail.com>
2023-03-06 23:47:37 -0700

    moved some things

--- login.js
+++ login.js
@@ -1,17 +1,5 @@
 class Login
 {
-    static login(uname)
-    {
-        if (document.cookie != '')
-        {
-            return this.#existingUser(uname);
-        }
-        else
-        {
-            this.#createUser(uname);
-        }
-    }
-
     #getCookies()
     {
         return document.cookie.split(';');
@@ -36,4 +24,52 @@ class Login
 
         return unameCookie[1];
     }
+
+    static login()
+    {
+        const unameElem = document.getElementById('username')
+        if (document.cookie != '')
+        {
+            let cookies = document.cookie.split(';');
+            let unameCookie = null;
+            for (let i = 0; i < cookies.length; i++)
+            {
+                if (cookies[i].includes("uname"))
+                {
+                    unameCookie = cookies[i].split('=');
+                }
+            }
+
+            return unameCookie[1];
+        }
+        else
+        {
+            document.cookie = `uname=${unameElem.value}`;
+        }
+    }
+
+    static getUser()
+    {
+        let cookies = this.#getCookies();
+        let unameCookie = null;
+        for (let i = 0; i < cookies.length; i++)
+        {
+            if (cookies[i].includes("uname"))
+            {
+                unameCookie = cookies[i].split('=');
+            }
+        }
+
+        return unameCookie[1];
+    }
+}
+
+function onLoginLoad()
+{
+    
+}
+
+function loginButton()
+{
+    Login.login();
 }
--- scores.js
+++ scores.js
@@ -9,4 +9,21 @@ class Scores
     {
         
     }
+
+    #getScoreCookie()
+    {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++)
+        {
+            if (cookies[i].includes('scores'))
+            {
+                return cookies[i].split('=');
+            }
+        }
+    }
+
+    #scoreJson(jsonString)
+    {
+        return JSON.parse(jsonString);
+    }
 }
